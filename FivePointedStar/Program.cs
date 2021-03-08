10ec6f835d21d05e4d4eea8aafa112d7d4b5ca6b
Andrey Valchuk <immigrationimmigrant@gmail.com>
2021-03-08 21:46:16 +0300

    Add project files.

---
+++ FivePointedStar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace FivePointedStar
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Введите значение длины стороны правильной пятиконечной звезды: ");
+
+            if (Double.TryParse(Console.ReadLine(), out double length))
+            {
+                FivePointedStar star = new FivePointedStar(length);
+
+                Console.WriteLine($"Result:\n{star}");
+            }
+            else
+            {
+                Console.WriteLine("Fatal error...");
+            }
+        }
+    }
+}
